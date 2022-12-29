042a4f15b6d62b53152a8842a6cafcf76823ef1a
Kevin Mas <kmas@thess-corp.fr>
2022-12-29 06:59:48 +0100

    fix: changing backend server

--- js/product.js
+++ js/product.js
@@ -8,12 +8,12 @@ const quantity = document.getElementById("quantity");
 const addToCart = document.getElementById("addToCart");
 
 // Creating variable for id of product
-const serverUrl = "https://mkwebd-kanap-back.cyclic.app/api/products/";
+const serverUrl = "https://Kanapback.kevinmas.repl.co/api/products/";
 const id = new URLSearchParams(window.location.search).get("id");
 
 // Create listener for addToCart
 addToCart.addEventListener("click", function () {
-  addItemToCart(productData);
+	addItemToCart(productData);
 });
 
 // Create variable with product data
@@ -21,92 +21,86 @@ let productData;
 
 // Get data from single object from API
 fetch(`${serverUrl}${id}`)
-  .then(function (res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function (data) {
-    productData = data;
-    // Add content to page
-    createContent(data);
-    title.innerHTML = data.name;
-    price.innerHTML = data.price;
-    description.innerHTML = data.description;
-    addColors(data);
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+	.then(function (res) {
+		if (res.ok) {
+			return res.json();
+		}
+	})
+	.then(function (data) {
+		productData = data;
+		// Add content to page
+		createContent(data);
+		title.innerHTML = data.name;
+		price.innerHTML = data.price;
+		description.innerHTML = data.description;
+		addColors(data);
+	})
+	.catch(function (err) {
+		console.log(err);
+	});
 
 // ********************************************************************************
 // Function to map from API data (unique product)
 // ********************************************************************************
 function createContent(datas) {
-  const productImg = `<img src="${datas.imageUrl}" alt="${datas.altTxt}">`;
-  image.insertAdjacentHTML("beforeend", productImg);
+	const productImg = `<img src="${datas.imageUrl}" alt="${datas.altTxt}">`;
+	image.insertAdjacentHTML("beforeend", productImg);
 }
 
 // ********************************************************************************
 // Function to add all colors
 // ********************************************************************************
 function addColors(data) {
-  for (let color of data.colors) {
-    let element = document.createElement("option");
-    element.setAttribute("value", color);
-    element.innerHTML = color;
-    colors.appendChild(element);
-  }
+	for (let color of data.colors) {
+		let element = document.createElement("option");
+		element.setAttribute("value", color);
+		element.innerHTML = color;
+		colors.appendChild(element);
+	}
 }
 
 // ********************************************************************************
 // Function for populating Local Storage
 // ********************************************************************************
 function addItemToCart(data) {
-  // Get values from localStorage & create storage if not created
-  let existingStorage = JSON.parse(window.localStorage.getItem("allCouches"));
-  if (existingStorage == null) {
-    existingStorage = [];
-  }
-  // Function to update Local Storage
-  function updateStorage() {
-    window.localStorage.setItem("allCouches", JSON.stringify(existingStorage));
-  }
-  // Create object with datas from selected item
-  let object = {
-    id: data._id,
-    color: colors.value,
-    quantity: parseInt(quantity.value),
-  };
+	// Get values from localStorage & create storage if not created
+	let existingStorage = JSON.parse(window.localStorage.getItem("allCouches"));
+	if (existingStorage == null) {
+		existingStorage = [];
+	}
+	// Function to update Local Storage
+	function updateStorage() {
+		window.localStorage.setItem("allCouches", JSON.stringify(existingStorage));
+	}
+	// Create object with datas from selected item
+	let object = {
+		id: data._id,
+		color: colors.value,
+		quantity: parseInt(quantity.value),
+	};
 
-  // Check if options are selected
-  if (object.color === "" || object.quantity === 0 || object.quantity > 100) {
-    alert(
-      "Veuillez sélectionner une couleur et/ou renseigner une quantité entre 1 et 100"
-    );
-  } else {
-    // Check if element exists in localStorage
-    if (
-      existingStorage.find(
-        (element) => element.color === object.color && element.id === object.id
-      )
-    ) {
-      // Get index of existing element in localStorage
-      let indexValue = existingStorage.findIndex(
-        (element) => element.color === object.color && element.id === object.id
-      );
+	// Check if options are selected
+	if (object.color === "" || object.quantity === 0 || object.quantity > 100) {
+		alert("Veuillez sélectionner une couleur et/ou renseigner une quantité entre 1 et 100");
+	} else {
+		// Check if element exists in localStorage
+		if (existingStorage.find((element) => element.color === object.color && element.id === object.id)) {
+			// Get index of existing element in localStorage
+			let indexValue = existingStorage.findIndex(
+				(element) => element.color === object.color && element.id === object.id
+			);
 
-      // Increment value of stored element
-      existingStorage[indexValue].quantity += object.quantity;
+			// Increment value of stored element
+			existingStorage[indexValue].quantity += object.quantity;
 
-      // Update localStorage with incremented value
-      updateStorage();
-      window.location.href = "cart.html";
-    } else {
-      // Add object to localStorage
-      existingStorage.push(object);
-      updateStorage();
-      window.location.href = "cart.html";
-    }
-  }
+			// Update localStorage with incremented value
+			updateStorage();
+			window.location.href = "cart.html";
+		} else {
+			// Add object to localStorage
+			existingStorage.push(object);
+			updateStorage();
+			window.location.href = "cart.html";
+		}
+	}
 }
--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 // Creating variable for items to inject
 const queryItems = document.getElementById("items");
-const serverUrl = "https://mkwebd-kanap-back.cyclic.app/api/products/";
+const serverUrl = "https://Kanapback.kevinmas.repl.co/api/products/";
 
 // ********************************************************************************
 // Getting data from API
 // ********************************************************************************
 fetch(serverUrl)
-  .then(function (res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function (data) {
-    createContent(data);
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+	.then(function (res) {
+		if (res.ok) {
+			return res.json();
+		}
+	})
+	.then(function (data) {
+		createContent(data);
+	})
+	.catch(function (err) {
+		console.log(err);
+	});
 
 // ********************************************************************************
 // Mapping function from API data
 // ********************************************************************************
 function createContent(item) {
-  for (let i = 0; i < item.length; i++) {
-    // Creating card for product
-    const productItem = `<a href="./product.html?id=${item[i]._id}">
+	for (let i = 0; i < item.length; i++) {
+		// Creating card for product
+		const productItem = `<a href="./product.html?id=${item[i]._id}">
 <article>
   <img src="${item[i].imageUrl}" alt="${item[i].altTxt}">
   <h3 class="productName">${item[i].name}</h3>
   <p class="productDescription">${item[i].description}</p>
 </article>
 </a>`;
-    // Insert template into DOM
-    queryItems.insertAdjacentHTML("beforeend", productItem);
-  }
+		// Insert template into DOM
+		queryItems.insertAdjacentHTML("beforeend", productItem);
+	}
 }
